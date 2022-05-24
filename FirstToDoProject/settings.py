125d8cdc4cbc9a83971f84c458f6a227c0d819e5
urvisuthar85 <urvisuthar85@gmail.com>
2022-05-24 20:44:44 +0530

    first commit1

--- FirstToDoProject/settings.py
+++ FirstToDoProject/settings.py
@@ -128,5 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'urvisuthar@gmail.com'
-EMAIL_HOST_PASSWORD = 'Urvi@1234'
+EMAIL_HOST_PASSWORD = 'PWD'
 EMAIL_PORT = 587
